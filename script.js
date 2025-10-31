fc6ab18e4c1641c9a78cd30f18e644d8b5e0f108
jordanayo <jordanboxall@outlook.com>
2025-10-31 02:00:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const splash = document.getElementById('splash');
 const enterBtn = document.getElementById('enter-btn');
 
+
 enterBtn.addEventListener('click', () => {
 	splash.style.transition = 'opacity 0.6s ease';
 	splash.style.opacity = '0';
+	enterBtn.style.transform = 'scale(1.2)';
 	setTimeout(() => {
 		splash.style.display = 'none';
+		header.style.display = 'flex'
 	}, 600);
+
 });
