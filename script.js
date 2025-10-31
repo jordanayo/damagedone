5307429667abe20147df48143117d74a6cf58863
jordanayo <jordanboxall@outlook.com>
2025-10-31 06:56:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const splash = document.getElementById('splash');
 const enterBtn = document.getElementById('enter-btn');
+const videoContainer = document.querySelector('.video-container');
+const slitty = document.querySelector('.slitty');
 
 
 enterBtn.addEventListener('click', () => {
 	splash.style.transition = 'opacity 0.6s ease';
 	splash.style.opacity = '0';
-	enterBtn.style.transform = 'scale(1.2)';
+	enterBtn.style.transform = 'scale(1.4)';
 	setTimeout(() => {
 		splash.style.display = 'none';
-		header.style.display = 'flex'
+		videoContainer.style.opacity = '1';
+		videoContainer.style.transform = 'scale(1)';
+		slitty.style.opacity = '1';
+		slitty.style.transform = 'scale(1)';
 	}, 600);
 
 });
+
